47bb99266d5c9c45404c83e96f65443761269939
OliverTavasz <->
2025-08-19 12:10:49 +0200

    Added handler class

---
+++ SalesSystem/Handler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SalesSystem
+{
+    public static class Handler
+    {
+        public static void Load()
+        {
+            // load movie data from file
+        }
+    }
+}
