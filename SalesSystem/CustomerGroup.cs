53c889298b4616af690254aa5fb61b32284be312
Jonatan Ljung <jonatan.ljung@hotmail.com>
2025-08-20 13:12:05 +0200

    Set up basic customerGroup object

--- SalesSystem/CustomerGroup.cs
+++ SalesSystem/CustomerGroup.cs
@@ -1,28 +1,39 @@
 using System.Security.Cryptography;
+using SalesSystem.Interfaces;
 using SalesSystem.Models;
 
 namespace SalesSystem;
 
 public class CustomerGroup {
-	List<Customer> customers = new List<Customer>();
-	ShoppingCart shoppingCart = new ShoppingCart();
+	public List<Customer> customers { get; } = [];
+	public ShoppingCart shoppingCart { get; } = new ShoppingCart();
 
+	// TODO only for testing purposes. Will be rewritten
 	public CustomerGroup() {
 		for (int i = 0; i < 3; i++) {
-			customers.Add(new Customer(RandomNumberGenerator.GetInt32(0, 101)));
+			this.AddCustomer(new Customer(RandomNumberGenerator.GetInt32(0, 101)));
 		}
-		shoppingCart.AddItem(new Ticket("Ticket", "", 105, 105, 25));
-		shoppingCart.AddItem(new Ticket("Ticket", "", 105, 105, 25));
-		shoppingCart.AddItem(new Snack("Ahlgrens bilar", "125 g", 19.64,22, 12));
-		shoppingCart.AddItem(new Snack("Ahlgrens bilar", "125 g", 19.64,22, 12));
+		this.AddShoppingCartItem(new Ticket("Ticket", "", 105, 105, 25));
+		this.AddShoppingCartItem(new Ticket("Ticket", "", 105, 105, 25));
+		this.AddShoppingCartItem(new Snack("Ahlgrens bilar", "125 g", 19.64,22, 12));
+		this.AddShoppingCartItem(new Snack("Ahlgrens bilar", "125 g", 19.64,22, 12));
 	}
 
+	// TODO only for testing purposes. Will be removed
 	public void PrintGroup() {
 		customers.ForEach(customer => {
 			Console.WriteLine(customer.Age);
 		});
-		shoppingCart.GetItems().ForEach(salesItem => {
+		shoppingCart.salesItems.ForEach(salesItem => {
 			Console.WriteLine(salesItem.GetType().Name);
 		});
+	}
+
+	public void AddCustomer(Customer newCustomer) {
+		customers.Add(newCustomer);
+	}
+
+	public void AddShoppingCartItem(ISalesItem salesItem) {
+		shoppingCart.salesItems.Add(salesItem);
 	}
 }
--- SalesSystem/Models/ShoppingCart.cs
+++ SalesSystem/Models/ShoppingCart.cs
@@ -3,18 +3,6 @@ using SalesSystem.Interfaces;
 namespace SalesSystem.Models;
 
 public class ShoppingCart {
-	private List<ISalesItem> shoppingCart { get; set; } = [];
-	private DateTime date { get; set; }  = DateTime.Now;
-
-	public void AddItem(ISalesItem item) {
-		this.shoppingCart.Add(item);
-	}
-
-	public List<ISalesItem> GetItems() {
-		return this.shoppingCart;
-	}
-
-	public void RemoveItem(ISalesItem item) {
-		this.shoppingCart.Remove(item);
-	}
+	public List<ISalesItem> salesItems { get; private set; } = [];
+	public DateTime date { get; private set; } = DateTime.Now;
 }
