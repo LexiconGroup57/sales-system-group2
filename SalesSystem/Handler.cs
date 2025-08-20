82ef97a1d821e58da5e8b7227a6f3f2c6d3f17b6
OliverTavasz <->
2025-08-20 13:02:07 +0200

    Integrated SeatArrangement into order-making

--- SalesSystem/Handler.cs
+++ SalesSystem/Handler.cs
@@ -4,6 +4,7 @@ namespace SalesSystem
 {
     public static class Handler
     {
+        private static SeatArrangement seatArrangement = new();
         public static void Load()
         {
             // load movie data from file
@@ -13,10 +14,18 @@ namespace SalesSystem
         public static void MakeOrder()
         {
             Console.Clear();
-            while (true)
+            Console.WriteLine("Enter the size of the group");
+            int groupSize = int.Parse(Console.ReadLine());
+
+            int[] ChosenSeats = new int[groupSize];
+            for(int i = 0; i < groupSize; i++)
             {
-                // customer is asked to enter all their information (ages, seats, etc.)
+                Console.Clear();
+                Console.WriteLine("Choose seat for person " + (i+1).ToString());
+                ChosenSeats[i] = seatArrangement.checkAndFillSeat();
+                Console.ReadLine();
             }
+            
         }
     }
 }
