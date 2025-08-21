ef28fe8c0a8a5a2e44d3c2e1be70d36da1beb055
OliverTavasz <->
2025-08-21 15:40:57 +0200

    attempted to merge MenuAndHandler into main

--- SalesSystem/Models/Snack.cs
+++ SalesSystem/Models/Snack.cs
@@ -8,5 +8,5 @@ public class Snack(string name, string description, double basePrice, double vat
 	public string Description { get; set; } = description;
 	public double BasePrice { get; set; } = basePrice;
 	public double Vat { get; set; } = vat;
-	public double VatPrice { get; set; } = Math.Round(basePrice * (1 + vat / 100));
+	public double VatPrice { get; set; } = Math.Round(basePrice * (1 + vat));
 }
